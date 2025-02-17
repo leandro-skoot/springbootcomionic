761950b89af73a9cf42074fa3936f760a02f52a4
Leandro Jose <leandro.skoot@gmail.com>
2025-02-17 17:44:28 -0300

    Estado e cidade

--- src/main/java/com/leandroskoot/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/leandroskoot/cursomc/repositories/CidadeRepository.java
@@ -3,9 +3,9 @@ package com.leandroskoot.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.leandroskoot.cursomc.domain.Categoria;
+import com.leandroskoot.cursomc.domain.Cidade;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{	
-
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
 }
--- src/main/java/com/leandroskoot/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/leandroskoot/cursomc/repositories/EstadoRepository.java
@@ -3,9 +3,9 @@ package com.leandroskoot.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.leandroskoot.cursomc.domain.Categoria;
+import com.leandroskoot.cursomc.domain.Estado;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{	
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{	
 
 }
