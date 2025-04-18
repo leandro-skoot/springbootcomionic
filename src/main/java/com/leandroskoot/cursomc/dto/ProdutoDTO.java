f5cf5b8d12b3c908c2d067ff3060baa68f6fba17
Leandro Jose <leandro.skoot@gmail.com>
2025-04-18 20:25:51 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/leandroskoot/cursomc/dto/ProdutoDTO.java
+++ src/main/java/com/leandroskoot/cursomc/dto/ProdutoDTO.java
@@ -2,28 +2,24 @@ package com.leandroskoot.cursomc.dto;
 
 import java.io.Serializable;
 
-import jakarta.validation.constraints.NotEmpty;
+import com.leandroskoot.cursomc.domain.Produto;
 
-import org.hibernate.validator.constraints.Length;
-
-import com.leandroskoot.cursomc.domain.Categoria;
-
-public class CategoriaDTO implements Serializable {
+public class ProdutoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
-	@NotEmpty(message = "Preenchimento obrigatório!")
-	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
-
-	public CategoriaDTO() {
+	private Double preco;
+	
+	public ProdutoDTO() {
 	}
 
-	public CategoriaDTO(Categoria obj) {
+	public ProdutoDTO(Produto obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		preco = obj.getPreco();
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -38,5 +34,13 @@ public class CategoriaDTO implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 }
